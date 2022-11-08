3ed3ddb70d32587ff964392935cf6f503cee7bd4
MatveyOleynikov <ochoba-1@mail.ru>
2022-11-08 21:11:28 +0300

    commented files because node js not start

--- front/script/get_all_exercises.js
+++ front/script/get_all_exercises.js
@@ -1,13 +1,15 @@
 // const {fetch} = require("fetch")
-const url = 'http://localhost:4000/api/exercises'
+//const url = 'http://localhost:4000/api/exercises'
 
-const new_exercise = async () => {
-    console.log('xxx ')
-    window.location.href = 'tasks.html';
+//const new_exercise = async () => {
+//    console.log('xxx ')
+//    //window.location.href = 'tasks.html';
+  //  const response = await fetch(url + "?" +  new URLSearchParams())
+   // console.log(JSON.stringify(response, null, 2))
     //const allExercises = ExerciseService.
-}
+//}
 
-const button = document.getElementById('add-task');
-console.log('ffff')
-console.log(button)
-button.onclick = new_exercise;
+//const button = document.getElementById('add-task');
+//console.log('ffff')
+//console.log(button)
+//button.onclick = new_exercise;8
