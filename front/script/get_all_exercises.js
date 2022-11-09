5416992322e400715793d4d72ea8700e17213530
MatveyOleynikov <ochoba-1@mail.ru>
2022-11-09 16:21:28 +0300

    add working all exercise list

--- front/script/get_all_exercises.js
+++ front/script/get_all_exercises.js
@@ -3,10 +3,26 @@ const url = 'http://localhost:4000/api/exercises' //нот фоунд (надо 
 
 const new_exercise = async () => {
     console.log('xxx ')
-    //window.location.href = 'tasks.html';
     const response = await fetch(url);
     result = await response.json();
-    console.log(result)
+    //window.location.href = 'tasks.html';
+
+    document.querySelector('#content-tasks-and-trains').innerHTML = "<article class=\"main-information-tasks\"><h1 style=\"text-align: center;\">Список упражнений</h1><div class=\"tasks\" id=\"tasks\">"
+
+    for (var key in result) {
+        //let dv = document.createElement('dv');
+        /*<div class="block-task">
+                <img src="images/task.jpg" alt="" class="task-image">
+                <button class="information-button">i</button>
+                <button class="plus-button">+</button>
+                <div class="description-task">Метод Арсения</div>
+            </div>*/
+        var new_element = "<div class=\"block-task\"> <img src=\"images/task.jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\" id=\"information-" + result[key].exercise_id + "\">i</button> <button class=\"plus-button\" id=\"plus-" + result[key].exercise_id + "\">+</button> <div class=\"description-task\"> " + result[key].name + " </div> </div>";
+        document.getElementById('tasks').innerHTML += new_element;
+        //document.querySelector('#tasks').append("<div class=\"block-task\"> <img src=\"images/task.jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i</button> <button class=\"plus-button\">+</button> <div class=\"description-task\">Метод Хуетод</div> </div>");
+        console.log(key, result[key].name)
+    }
+    //console.log(result)
 }
 
 const button = document.getElementById('add-task');
