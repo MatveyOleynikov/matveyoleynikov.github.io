7ed8a2b19086d93dd5129ae44606c595bb57c7f9
MatveyOleynikov <ochoba-1@mail.ru>
2022-11-28 11:03:46 +0300

    fix bug with url

--- front/script/get_all_exercises.js
+++ front/script/get_all_exercises.js
@@ -10,39 +10,12 @@ const new_exercise = async () => {
     document.querySelector('#content-tasks-and-trains').innerHTML = "<article class=\"main-information\"><h1 style=\"text-align: center;\">Список упражнений</h1><div class=\"tasks\" id=\"tasks\">"
 
     for (var key in result) {
-        //let dv = document.createElement('dv');
-        /*<div class="block-task">
-                <img src="images/task.jpg" alt="" class="task-image">
-                <button class="information-button">i</button>
-                <button class="plus-button">+</button>
-                <div class="description-task">Метод Арсения</div>
-            </div>*/
-        var new_element = "<div class=\"block-task\"> <img src=\"images/exercises/" + result[key].exercise_id + ".jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i <span class=\"information-buttontext\">" + result[key].description + "</span> </button> <button class=\"plus-button\" id=\"plus-" + result[key].exercise_id + "\" onclick=\"check_infa(this);\">+</button> <div class=\"description-task\"> " + result[key].name + " </div> </div>";
+        var new_element = "<div class=\"block-task\"> <img src=\" " + result[key].url + " \" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i <span class=\"information-buttontext\">" + result[key].description + "</span> </button> <button class=\"plus-button\" id=\"plus-" + result[key].exercise_id + "\" onclick=\"check_infa(this);\">+</button> <div class=\"description-task\"> " + result[key].name + " </div> </div>";
         document.getElementById('tasks').innerHTML += new_element;
         //document.querySelector('#tasks').append("<div class=\"block-task\"> <img src=\"images/task.jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i</button> <button class=\"plus-button\">+</button> <div class=\"description-task\">Метод Хуетод</div> </div>");
         console.log(key, result[key].name)
     }
 
-    for (var key in result) {
-        var new_element = "<div class=\"block-task\"> <img src=\"images/exercises/" + result[key].exercise_id + ".jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i <span class=\"information-buttontext\">" + result[key].description + "</span> </button> <button class=\"plus-button\" id=\"plus-" + result[key].exercise_id + "\" onclick=\"check_infa(this);\">+</button> <div class=\"description-task\"> " + result[key].name + " </div> </div>";
-        document.getElementById('tasks').innerHTML += new_element;
-        //document.querySelector('#tasks').append("<div class=\"block-task\"> <img src=\"images/task.jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i</button> <button class=\"plus-button\">+</button> <div class=\"description-task\">Метод Хуетод</div> </div>");
-        console.log(key, result[key].name)
-    }
-
-    for (var key in result) {
-        var new_element = "<div class=\"block-task\"> <img src=\"images/exercises/" + result[key].exercise_id + ".jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i <span class=\"information-buttontext\">" + result[key].description + "</span> </button> <button class=\"plus-button\" id=\"plus-" + result[key].exercise_id + "\" onclick=\"check_infa(this);\">+</button> <div class=\"description-task\"> " + result[key].name + " </div> </div>";
-        document.getElementById('tasks').innerHTML += new_element;
-        //document.querySelector('#tasks').append("<div class=\"block-task\"> <img src=\"images/task.jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i</button> <button class=\"plus-button\">+</button> <div class=\"description-task\">Метод Хуетод</div> </div>");
-        console.log(key, result[key].name)
-    }
-
-    for (var key in result) {
-        var new_element = "<div class=\"block-task\"> <img src=\"images/exercises/" + result[key].exercise_id + ".jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i <span class=\"information-buttontext\">" + result[key].description + "</span> </button> <button class=\"plus-button\" id=\"plus-" + result[key].exercise_id + "\" onclick=\"check_infa(this);\">+</button> <div class=\"description-task\"> " + result[key].name + " </div> </div>";
-        document.getElementById('tasks').innerHTML += new_element;
-        //document.querySelector('#tasks').append("<div class=\"block-task\"> <img src=\"images/task.jpg\" alt=\"\" class=\"task-image\"> <button class=\"information-button\">i</button> <button class=\"plus-button\">+</button> <div class=\"description-task\">Метод Хуетод</div> </div>");
-        console.log(key, result[key].name)
-    }
     var gotovo = "<button id=\"new-train\" onclick=\"new_train()\">Добавить в тренировку</button>";
     document.getElementById('tasks').innerHTML += gotovo;
     //console.log(result)
